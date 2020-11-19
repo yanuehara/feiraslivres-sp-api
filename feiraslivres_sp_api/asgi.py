970cf7d2e510e0da4fcfae51f4e27fd960443c98
Yan Uehara <yanuehara@hotmail.com>
2020-11-19 15:47:37 -0300

    Creates initial project with django-admin
    Signed-off-by: Yan Uehara <yanuehara@hotmail.com>

---
+++ feiraslivres_sp_api/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for feiraslivres_sp_api project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'feiraslivres_sp_api.settings')
+
+application = get_asgi_application()
