a8a2be89d23caba564bdeac6ca8473c6116ab722
Yan Uehara <yanuehara@hotmail.com>
2020-11-20 14:21:36 -0300

    Changes DB to postgreSQL
    Signed-off-by: Yan Uehara <yanuehara@hotmail.com>

--- feiraslivres_sp_api/settings.py
+++ feiraslivres_sp_api/settings.py
@@ -74,10 +74,15 @@ WSGI_APPLICATION = 'feiraslivres_sp_api.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+	'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'feiraapidb',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
+
 }
 
 
